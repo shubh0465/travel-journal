62ecebe2254f516a99dcfff648bc441ff8c7cd76
shubh0465 <55250438+shubh0465@users.noreply.github.com>
2023-01-26 18:01:20 +0530

    Header added

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header'>
+        <img src="/images/Head.png" alt="" />
+        <span>my travel Journal</span>
+    </div>
+  )
+}
